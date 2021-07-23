c9e8205f5fa4e71f16f4d9aad925f30ec428319c
dylan <dylanamiller3@gmail.com>
2021-07-23 19:06:31 -0400

    write actor critic

--- jaxrl/agents/model_free/a2c.py
+++ jaxrl/agents/model_free/a2c.py
@@ -1,75 +1,130 @@
-import time
+from typing import Any, Callable, NamedTuple, Tuple
 
+from bsuite.baselines import base
+from bsuite.baselines.utils import sequence
+
+import dm_env
+from dm_env import specs
+import haiku as hk
+import jax
 import jax.numpy as jnp
-from jax import grad, jit
+import optax
+import rlax
 
-from jaxrl.agents.base.pg_base import PGBase
-from jaxrl.approx.linear import Linear
-from jaxrl.policies import Discrete
-from jaxrl.utils import set_seed
+Logits = jnp.ndarray
+Value = jnp.ndarray
+PolicyValueNet = Callable[[jnp.ndarray], Tuple[Logits, Value]]
 
 
-class A2C(PGBase):
-    def __init__(self, env, approx=Linear, policy=Discrete, seed=0, **kwargs):
-        set_seed(seed)
+class TrainingState(NamedTuple):
+  params: hk.Params
+  opt_state: Any
 
-        self.approx = approx(env, kwargs)
-        self.policy = policy(env, kwargs)
-        self.critic = approx(env, kwargs, out=1)
 
-    def act(self, obs, action=None):
-        logits = self.approx(obs)
-        action, log_prob, entropy = self.policy(logits)
-        value = self.critic(logits)
-        return action, value, log_prob, entropy
+class ActorCritic(base.Agent):
+  """Feed-forward actor-critic agent."""
 
-    def train(self, rollout_data):
-        """
-        Computes the actor, critic, and entropy losses for training. Uses the stored rollout and the recieved
-        rewards, dones, and next_observation from the environment
-        """
-        # convert lists into torch tensors for ease of manipulation and vectorized operations
-        # resultant shape is [seq, batch, ...] meaning [rollout_len, # vector envs, ...]
-        # torch stack rollout features
-        values = rollout_data['values']
-        log_probs_action = rollout_data['log_probs']
-        entropies = rollout_data['entropies']
-        
-        # these are list[list[float|bool]] so shape is [seq, 2]
-        # move to whatever device the values are on to support GPU or CPU
-        rewards = torch.from_numpy(np.asarray(rewards)).to(values.device)
-        dones = torch.from_numpy(np.asarray(dones)).to(values.device)
+  def __init__(
+      self,
+      obs_spec: specs.Array,
+      action_spec: specs.DiscreteArray,
+      network: PolicyValueNet,
+      optimizer: optax.GradientTransformation,
+      rng: hk.PRNGSequence,
+      sequence_length: int,
+      discount: float,
+      td_lambda: float,
+  ):
 
-        # estimate value of next state and TD *without* gradients. This is the target and shouldn't have grads
-        with torch.no_grad():
-            predictions = network(next_obs)
-            next_values = predictions["critic"].squeeze()
+    # Define loss function.
+    def loss(trajectory: sequence.Trajectory) -> jnp.ndarray:
+      """"Actor-critic loss."""
+      logits, values = network(trajectory.observations)
+      td_errors = rlax.td_lambda(
+          v_tm1=values[:-1],
+          r_t=trajectory.rewards,
+          discount_t=trajectory.discounts * discount,
+          v_t=values[1:],
+          lambda_=jnp.array(td_lambda),
+      )
+      critic_loss = jnp.mean(td_errors**2)
+      actor_loss = rlax.policy_gradient_loss(
+          logits_t=logits[:-1],
+          a_t=trajectory.actions,
+          adv_t=td_errors,
+          w_t=jnp.ones_like(td_errors))
 
-            # compute nstep return AKA the temporal difference target
-            target_values = self.compute_td_target(next_values, rewards, dones)
-            # compute the temporal difference AKA the advantage for each step
-            # use .detach() to make sure this does not store gradients, should be covered by torch.no_grad() though
-            advantages = target_values - values.detach()
-            
-            # GOTCHA check, make sure torch does not do any broadcasting and shapes are what we expect
-            assert target_values.shape == values.shape
-            assert advantages.ndim == 2
+      return actor_loss + critic_loss
 
-        # compute the losses vectorized, shape is [sequence, batch]
-        # GOTCHA checks, make sure torch does not do any broadcasting and shapes are what we expect
-        # do this before meaning, otherwise the dimensions are lost
-        assert log_probs_action.shape == advantages.shape
-        policy_loss = -log_probs_action * advantages
-        assert policy_loss.ndim == 2
-        
-        entropy_loss = -entropies * self.entropy_weight
-        assert entropy_loss.ndim == 2
-        
-        # squared error
-        assert target_values.shape == values.shape
-        value_loss = (target_values - values) ** 2
-        assert value_loss.ndim == 2
-        
-        # mean over seq, batch to create a single loss, critic is half (0.5) of the other losses
-        loss = 0.5 * value_loss.mean() + policy_loss.mean() + entropy_loss.mean()
-        return loss
+    # Transform the loss into a pure function.
+    loss_fn = hk.without_apply_rng(hk.transform(loss, apply_rng=True)).apply
+
+    # Define update function.
+    @jax.jit
+    def sgd_step(state: TrainingState,
+                 trajectory: sequence.Trajectory) -> TrainingState:
+      """Does a step of SGD over a trajectory."""
+      gradients = jax.grad(loss_fn)(state.params, trajectory)
+      updates, new_opt_state = optimizer.update(gradients, state.opt_state)
+      new_params = optax.apply_updates(state.params, updates)
+      return TrainingState(params=new_params, opt_state=new_opt_state)
+
+    # Initialize network parameters and optimiser state.
+    init, forward = hk.without_apply_rng(hk.transform(network, apply_rng=True))
+    dummy_observation = jnp.zeros((1, *obs_spec.shape), dtype=jnp.float32)
+    initial_params = init(next(rng), dummy_observation)
+    initial_opt_state = optimizer.init(initial_params)
+
+    # Internalize state.
+    self._state = TrainingState(initial_params, initial_opt_state)
+    self._forward = jax.jit(forward)
+    self._buffer = sequence.Buffer(obs_spec, action_spec, sequence_length)
+    self._sgd_step = sgd_step
+    self._rng = rng
+
+  def select_action(self, timestep: dm_env.TimeStep) -> base.Action:
+    """Selects actions according to a softmax policy."""
+    key = next(self._rng)
+    observation = timestep.observation[None, ...]
+    logits, _ = self._forward(self._state.params, observation)
+    action = jax.random.categorical(key, logits).squeeze()
+    return int(action)
+
+  def update(
+      self,
+      timestep: dm_env.TimeStep,
+      action: base.Action,
+      new_timestep: dm_env.TimeStep,
+  ):
+    """Adds a transition to the trajectory buffer and periodically does SGD."""
+    self._buffer.append(timestep, action, new_timestep)
+    if self._buffer.full() or new_timestep.last():
+      trajectory = self._buffer.drain()
+      self._state = self._sgd_step(self._state, trajectory)
+
+
+def default_agent(obs_spec: specs.Array,
+                  action_spec: specs.DiscreteArray,
+                  seed: int = 0) -> base.Agent:
+  """Creates an actor-critic agent with default hyperparameters."""
+
+  def network(inputs: jnp.ndarray) -> Tuple[Logits, Value]:
+    flat_inputs = hk.Flatten()(inputs)
+    torso = hk.nets.MLP([64, 64])
+    policy_head = hk.Linear(action_spec.num_values)
+    value_head = hk.Linear(1)
+    embedding = torso(flat_inputs)
+    logits = policy_head(embedding)
+    value = value_head(embedding)
+    return logits, jnp.squeeze(value, axis=-1)
+
+  return ActorCritic(
+      obs_spec=obs_spec,
+      action_spec=action_spec,
+      network=network,
+      optimizer=optax.adam(3e-3),
+      rng=hk.PRNGSequence(seed),
+      sequence_length=32,
+      discount=0.99,
+      td_lambda=0.9,
+  )
