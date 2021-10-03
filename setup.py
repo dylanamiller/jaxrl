dcf1bb1cde529f04bb7ff4bf1ef2bd904655cd43
dylan <dylanamiller3@gmail.com>
2021-10-03 09:17:27 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jaxrl',
+    version='0.0.dev1',
+    author='Dylan Miller',
+    description='rl library',
+    license='Open',
+    python_requires='>=3.8.0',
+    packages=find_packages()
+)
